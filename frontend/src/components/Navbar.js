f9c11000558deb9bcbb02dfffc35aee6424b6efc
Antonio <a.mazzara@st.hanze.nl>
2025-03-01 13:08:25 +0100

    link to predict page on the navbar (mobile)

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -41,6 +41,12 @@ import React, { useContext, useState } from "react";
             >
               Spelkalender
             </Link>
+            <Link
+              to="/predict-result"
+              className="hover:text-blue-400 transition duration-200"
+            >
+              Predict
+            </Link>
 
             {/* Links based on user roles */}
             {authState.isLoggedIn && authState.role === "beheer" && (
@@ -122,7 +128,8 @@ import React, { useContext, useState } from "react";
             </Link>
             <Link
               to="/predict-result"
-              className="hover:text-blue-400 transition duration-200"
+              className="block py-2 px-4 hover:bg-gray-600 rounded"
+              onClick={() => setIsMenuOpen(false)}
             >
               Predict
             </Link>
